5b7733e0add11c623a8cb689f48ea900f7d14e12
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-02-23 21:23:24 -0500

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,10 @@
-#!/usr/bin/pup
 # Install an version of flask (2.1.0)
 package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
+}
+
+package {'werkseug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
