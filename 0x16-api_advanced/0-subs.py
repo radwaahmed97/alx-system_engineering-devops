1f88eefae51d0a6f0b05ad0ef93904568993263e
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-04-09 21:01:31 -0400

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,19 +10,11 @@ def number_of_subscribers(subreddit):
     (not active users, total subscribers) for a given subreddit.
     If an invalid subreddit is given, the function should return 0.
     """
-    main_url = 'https://www.reddit.com/r/'
-
-    url = '{}{}/about.json'.format(main_url, subreddit)
-    headers = {
-        'User-Agent':
-        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
-        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
-    }
-    results = requests.get(
-        url,
-        headers=headers,
-        allow_redirects=False
-    )
-    if results.status_code == 200:
-        return results.json()['data']['subscribers']
-    return 0
+    URL = 'https://www.reddit.com/r/'
+    res = requests.get('{}{}/about.json'.
+                       format(URL, subreddit),
+                       headers={'User-Agent': 'ALX-User-Agent'},
+                       allow_redirects=False)
+    if res.status_code != 200:
+        return 0
+    return res.json().get('data').get('subscribers')
